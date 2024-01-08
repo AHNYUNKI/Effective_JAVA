f9379ae30e1fb987ee15ed778bf54f35bc2ee201
AHNYUNKI <ahnyunki@naver.com>
2024-01-08 13:22:08 +0900

    Singleton의 종류 및 장점들

--- src/item_03/singleton_private/Elvis.java
+++ src/item_03/singleton_private/Elvis.java
@@ -1,12 +1,14 @@
 package item_03.singleton_private;
 
+/**
+ * 장점 - 해당 클래스가 싱글턴임이 API에 명확히 드러난다는 것이다.
+ *       public static 필드가 final이니 절대로 다른 객체를 참조 할 수 없다.
+ *       그리고 간결하다.
+ */
 public class Elvis {
-    private static final Elvis INSTANCE = new Elvis();
+    public static final Elvis INSTANCE = new Elvis();
     private Elvis() {
         // ...
-    }
-    public static Elvis getInstance() {
-        return INSTANCE;
     }
 
     public void leaveTheBuilding() {
--- src/item_03/singleton_public/Elvis.java
+++ src/item_03/singleton_public/Elvis.java
@@ -1,9 +1,19 @@
 package item_03.singleton_public;
 
+/**
+ * 장점 - API를 바꾸지 않고도 싱글톤이 아니게 변경할 수 있다는 점이다.
+ *       유일한 인스턴스를 반환하던 팩터리 메서드가 호출하는 스레드별로 다른 인스턴스를 넘겨주게 할 수 있다.
+ *       또, 원한다면 정적 팩터리를 제네릭 싱글턴 팩터리로 만들 수 있다는 점이다.
+ *       그리고 마지막으로 정적 팩터리의 메서드 참조를 공급자로 사용할 수 있다는 점이다.
+ *       가령 Elvis::getInstance를 Supplier<Elvis>로 사용하ㄴㄴ 식이다.
+ */
 public class Elvis {
-    public static final Elvis INSTANCE = new Elvis();
+    private static final Elvis INSTANCE = new Elvis();
     private Elvis() {
         // ...
+    }
+    public static Elvis getInstance() {
+        return INSTANCE;
     }
 
     public void leaveTheBuilding() {
